69ae7de73fdc87a1a98780c8b03b62a37e851c72
wormrise <wormrisee@gmail.com>
2025-10-04 17:59:06 +0300

    Update index.php

--- users/index.php
+++ users/index.php
@@ -12,8 +12,26 @@
         },
          {
             "id": 2,
-            "cliente_NOMBRE": "YT",
-            "cliente_ID": "gg_116152060195741273040",
+            "cliente_NOMBRE": "ege",
+            "cliente_ID": "gg_109593757648223008668",
+            "Client_KeyAccecs": "XTPRIVATESKIN",
+            "cliente_DateStarted": "04-02-2025",
+            "cliente_DateExpired": "04-06-2028",
+            "status": 1
+        },
+        {
+            "id": 3,
+            "cliente_NOMBRE": "frat",
+            "cliente_ID": "gg_116892310186425257822",
+            "Client_KeyAccecs": "XTPRIVATESKIN",
+            "cliente_DateStarted": "04-02-2025",
+            "cliente_DateExpired": "04-06-2028",
+            "status": 1
+        },
+        {
+            "id": 3,
+            "cliente_NOMBRE": "",
+            "cliente_ID": "",
             "Client_KeyAccecs": "XTPRIVATESKIN",
             "cliente_DateStarted": "04-02-2025",
             "cliente_DateExpired": "04-06-2028",
@@ -21,3 +39,4 @@
         }
     ]
 }
+
